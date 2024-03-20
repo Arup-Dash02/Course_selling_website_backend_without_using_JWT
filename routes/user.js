850ac8375ead4eddd16b7a6a5cc5255be56dad0c
Arup-Dash02 <dasharup559@gmail.com>
2024-03-20 20:32:50 +0530

    changed in user route

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,3 @@
-// const { Router } = require("express");
-// const router = Router();
-// const userMiddleware = require("../middleware/user");
-
-// // User Routes
-// router.post("/signup", (req, res) => {
-//   // Implement user signup logic
-// });
-
-// router.get("/courses", (req, res) => {
-//   // Implement listing all courses logic
-// });
-
-// router.post("/courses/:courseId", userMiddleware, (req, res) => {
-//   // Implement course purchase logic
-// });
-
-// router.get("/purchasedCourses", userMiddleware, (req, res) => {
-//   // Implement fetching purchased courses logic
-// });
-
-// module.exports = router;
-//------------------------------------------------------------------
 // const express = require("express");
 // const router = express.Router();
 
